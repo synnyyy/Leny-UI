dd7ee613d2bc3daed8800ee50e4612c7d3e684c4
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-12-27 11:21:07 +0000

    Update Navigation.lua

--- Modules/Navigation.lua
+++ Modules/Navigation.lua
@@ -1,12 +1,12 @@
 local Navigation = {}
 Navigation.__index = Navigation
 
-local Utility = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/L3nyFromV3rm/Leny-UI/refs/heads/main/Modules/Utility.lua", true))()
+local Utility = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/synnyyy/Leny-UI/refs/heads/main/Modules/Utility.lua", true))()
 local Popup = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/L3nyFromV3rm/Leny-UI/refs/heads/main/Modules/Popup.lua", true))()
 
 function Navigation.new(context: table)
 	local self = setmetatable(context, Navigation)
-	Utility:validateKeys(self, {"ScrollingFrame", "Pages", "Page", "tweenTabOn", "tweenTabsOff", "tweenTabsOff", "animation", "hoverOn"})
+	Utility:validateKeys(self, {"ScrollingFrame", "Pages", "Page", "tweenTabOn", "tweenTabsOff", "animation", "hoverOn", "hoverOff", "Popups"})
 	return self
 end
 
@@ -16,9 +16,11 @@ function Navigation:enableFirstTab()
 end
 
 function Navigation:selectTab()
+	local isSwitching = false
+
 	local function showPage()
 		for _, page in ipairs(self.Pages:GetChildren()) do
-			if string.match(page.Name, "Page") and page.Visible then
+			if string.match(page.Name, "Page") then
 				page.Visible = false
 			end
 		end
@@ -27,7 +29,7 @@ function Navigation:selectTab()
 		self.animation()
 	end
 
-	local function tweenTabs(onTween, offTweenCallback)
+	local function tweenTabs()
 		for _, tab in ipairs(self.ScrollingFrame:GetChildren()) do
 			if string.match(tab.Name, "Tab") then
 				self.tweenTabsOff(tab)
@@ -38,23 +40,27 @@ function Navigation:selectTab()
 	end
 
 	return function()
-		if self.Page.Visible then
+		if isSwitching or self.Page.Visible then
 			return
 		end
-		
+
+		isSwitching = true
 		Popup:hidePopups(false, self.Popups)
+
 		showPage()
 		tweenTabs()
+
+		isSwitching = false
 	end
 end
 
-function Navigation:hoverEffect(boolean)
-	return function()	
+function Navigation:hoverEffect(isHovering)
+	return function()
 		if self.Page.Visible then
 			return
 		end
 
-		if boolean then
+		if isHovering then
 			self.hoverOn()
 		else
 			self.hoverOff()
